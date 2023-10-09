3e14b4762bd5ca655990464d14f96148c3ff5cf0
TheoBueno <143668478+TheoBueno@users.noreply.github.com>
2023-10-08 22:24:09 -0400

    added icons, stops and popups

--- mapanimations.js
+++ mapanimations.js
@@ -1,6 +1,5 @@
 // TODO: Add your own Access Token key bellow:
-mapboxgl.accessToken =
-'Add your Own Access Key to Mapbox here: https://account.mapbox.com/';
+mapboxgl.accessToken = '' /*'Add your Own Access Key to Mapbox here: https://account.mapbox.com/'*/;
             
 //Makes the map and sets center position
 let map = new mapboxgl.Map({
@@ -9,6 +8,62 @@ style: 'mapbox://styles/mapbox/streets-v11',
 center: [-71.093229, 42.350544],
 zoom: 12.5,
 });
+
+const busStops = [
+    [-71.093729, 42.359244],
+    [-71.094915, 42.360175],
+    [-71.0958, 42.360698],
+    [-71.099558, 42.362953],
+    [-71.103476, 42.365248],
+    [-71.106067, 42.366806],
+    [-71.108717, 42.368355],
+    [-71.110799, 42.369192],
+    [-71.113095, 42.370218],
+    [-71.115476, 42.372085],
+    [-71.117585, 42.373016],
+    [-71.118625, 42.374863],
+];
+
+//GeoJSON Load Data
+const geojson = {
+    type: 'FeatureCollection',
+    features: []
+};
+
+function placeRouteStops(bSt) { 
+    bSt.forEach(i => {
+        geojson.features.push(
+            {
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: [i[0],i[1]]
+                },
+                properties: {
+                  title: 'Bus Stop',
+                  description: 'MBTA Bus Route 93'
+                }
+              },
+        )
+    });
+}
+
+placeRouteStops(busStops)
+
+// add markers to map
+for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = document.createElement('div');
+    el.className = 'marker';
+  
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(
+            `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+          )
+      ).addTo(map);
+}
 
 var allMarkers = [];
 var manyBuses = [];
@@ -47,6 +102,8 @@ console.log(manyBuses);
 setTimeout(() => {
 for (i = 0; i < manyBuses.length; i++) {
     allMarkers[i] = new mapboxgl.Marker()
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML(`<h3>Bus ${i}</h3><p>Bus number ${i} of the MBTA Bus Route 93</p>`))
         .setLngLat(manyBuses[i])
         .addTo(map);
 }
